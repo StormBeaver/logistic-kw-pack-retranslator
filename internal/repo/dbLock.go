03246535519cfeeee13f5a4ca76fa6cfdecd8672
Evgeniy Orekhov <super-nutman@yandex.ru>
2025-09-25 17:51:38 +0300

    change lock id

--- internal/repo/dbLock.go
+++ internal/repo/dbLock.go
@@ -6,20 +6,14 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-// lock for tx
-func AcquireLockTx(ctx context.Context, tx *sqlx.Tx) error {
-	_, err := tx.ExecContext(ctx, "SELECT pg_advisory_xact_lock(hashtext('txLock'))")
-	return err
-}
-
 // lock for regular sql requests
 func AcquireLock(ctx context.Context, db *sqlx.DB) error {
-	_, err := db.ExecContext(ctx, "SELECT pg_advisory_lock(hashtext('dbLock'))")
+	_, err := db.ExecContext(ctx, "SELECT pg_advisory_lock(523698741)")
 	return err
 }
 
 // unlock for regular sql requests
 func Unlock(ctx context.Context, db *sqlx.DB) error {
-	_, err := db.ExecContext(ctx, "SELECT pg_advisory_unlock(hashtext('dbLock'))")
+	_, err := db.ExecContext(ctx, "SELECT pg_advisory_unlock(523698741)")
 	return err
 }
